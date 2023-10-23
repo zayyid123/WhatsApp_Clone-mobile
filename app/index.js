5503c3c419d92a9ee5ee6e36613e00c155392098
zayyid123 <mochamadzayyid@gmail.com>
2023-10-23 09:49:20 +0700

    add heroicons

--- app/index.js
+++ app/index.js
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+import { SparklesIcon } from "react-native-heroicons/solid";
+
 const App = () => {
   return (
     <View
       className='bg-red-300'
     >
+      <SparklesIcon size={30} fill={'#000'}/>
       <Text>App</Text>
     </View>
   )
